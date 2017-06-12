01581aea9241c5581d57450f0f72e00c1437b85a
Cesar Sandrigo <cmsandrigo@gmail.com>
2017-06-12 18:18:10 -0300

    Printer service is working in a basic way

---
+++ PrinterServer.js
@@ -0,0 +1,30 @@
+//'use strict';
+
+const http = require("http");
+const Router = require('node-simple-router');
+const EventEmitter = require('events').EventEmitter;
+
+exports.PrinterServer = class PrinterServer extends EventEmitter {
+    constructor(port) {
+        super();
+        this.port = port;
+    }
+
+    init() {
+        this.emit('event', 'Initializing Printer Server...');
+        this.router = new Router();
+        this.router.get("/status", function(request, response) {
+            this.emit('event', 'Printer status requested');
+            var res = { status: 'idle' };
+            response.end(res);
+        });
+
+        this.server = http.createServer(this.router);
+        // server.on('request', function(request, response) {
+        // });
+        this.server.listen(this.port);
+        this.emit('event', 'PrinterServer listening on port ' + this.port);
+    }
+}
+
+exports.PrinterServer
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {app, Tray, Menu, BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
 
-const iconPath = path.join(__dirname, 'icon.png');
+const iconPath = path.join(__dirname, '/img/icon.png');
 let appIcon = null;
 let win = null;
 
